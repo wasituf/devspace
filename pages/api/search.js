95b227de0f3265fc354fc5fc4d2178972b24dd77
Wasit Faraizi <wasit.allin1@gmail.com>
2022-09-28 16:47:42 +0600

    implemented caching posts

--- pages/api/search.js
+++ pages/api/search.js
@@ -5,7 +5,8 @@ import matter from 'gray-matter'
 export default function handler(req, res) {
   let posts
   if (process.env.NODE_ENV === 'production') {
-    // TODO - fetch from cache
+    // Fetch from cache
+    posts = require('../../cache/data').posts
   } else {
     const files = fs.readdirSync(path.join('posts'))
 
